c5ac8adfaba9ea06b5d5afdc0dd4bfd8a685e1d9
onmax <maximogarciamtnez@gmail.com>
2026-01-30 18:53:48 +0100

    use vercel nitro preset

--- apps/website/nuxt.config.ts
+++ apps/website/nuxt.config.ts
@@ -1,5 +1,6 @@
 export default defineNuxtConfig({
   modules: ['@nuxt/ui', '@nuxt/fonts', '@nuxt/icon', '@vueuse/nuxt'],
+  nitro: { preset: 'vercel' },
   css: ['~/assets/css/main.css'],
   compatibilityDate: '2025-01-01',
   fonts: {
